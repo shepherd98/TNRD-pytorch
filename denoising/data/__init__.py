7fee50f37fc2f7232c7458cb3b33f77c0d5b09b9
尚臻 <zhushz@qq.com>
2022-07-05 00:26:45 +0800

    Update __init__.py

--- denoising/data/__init__.py
+++ denoising/data/__init__.py
@@ -8,7 +8,7 @@ def get_loaders(args):
     dataset_val = DatasetNoise(root=os.path.join(args.root, 'val'), noise_sigma=args.noise_sigma, training=False, crop_size=args.crop_size, blind_denoising=args.blind, gray_scale=args.gray_scale, max_size=args.max_size)
 
     # loaders
-    loader_train = DataLoader(dataset_train, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
+    loader_train = DataLoader(dataset_train, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers)
     loader_eval = DataLoader(dataset_val, batch_size=1, shuffle=False, num_workers=1)
     loaders = {'train': loader_train, 'eval': loader_eval}
 
